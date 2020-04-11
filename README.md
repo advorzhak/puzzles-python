e9842838defd9a268e08e6d8a759663544086ac8
Andrii Dvorzhak <dvorzhak@gmail.com>
2020-04-11 17:59:33 +0300

    Practice task 1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # puzzles-python
+## Practice task 1
+```bash
+python3.exe pairs.py 5 10 0 10 5 4.50 6.7 10.8 ''56.6'' 5.5 57..7 NaN czxczxczx dfgdfg5fdg
+"57..7" - isn't figure, ommited.
+"NaN" - isn't figure, ommited.
+"czxczxczx" - isn't figure, ommited.
+"dfgdfg5fdg" - isn't figure, ommited.
+All pairs:
+5.0 + 5.0 = 10
+10.0 + 0.0 = 10
+0.0 + 10.0 = 10
+4.5 + 5.5 = 10
+Unique pairs:
+5.0 + 5.0 = 10
+4.5 + 5.5 = 10
+0.0 + 10.0 = 10
+
+Process finished with exit code 0
+
+```
---
+++ pairs.py
@@ -0,0 +1,49 @@
+import sys
+import re
+
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def find_sums(inputs, sum_to_find=10):
+    i = 0
+    l = len(inputs)
+    pairs = list()
+    print("All pairs:")
+    while i < l - 1:
+        j = i + 1
+        while j < l:
+            if inputs[i] + inputs[j] == sum_to_find:
+                print("{0} + {1} = {2}".format(inputs[i], inputs[j], sum_to_find))
+                current_pair = [inputs[i], inputs[j]]
+                current_pair.sort()
+                pairs.append(current_pair)
+            j = j + 1
+        i = i + 1
+    print("Unique pairs:")
+    unique_pairs = set(tuple(current_pair) for current_pair in pairs)
+    for current_pair in unique_pairs:
+        print("{0} + {1} = {2}".format(current_pair[0], current_pair[1], sum_to_find))
+
+
+def main():
+    inputs = []
+    for current_input in sys.argv[1:]:
+        current_input_parsed = re.findall('^((\"|\'){0,2})([\d\.]*)((\"|\'){0,2})$', current_input)
+        if current_input_parsed and isfloat(current_input_parsed[0][2]):
+            if current_input_parsed != "NaN":
+                inputs.append(float(current_input_parsed[0][2]))
+            else:
+                print("\"{0}\" - is NaN, ommited.".format(current_input_parsed[0][2]))
+        else:
+            print("\"{0}\" - isn't figure, ommited.".format(current_input))
+    find_sums(inputs)
+
+
+if __name__ == "__main__":
+    main()
