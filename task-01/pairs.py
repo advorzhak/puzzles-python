dfe9176c56fd4a03caba62eed65d8ae885ab0d16
Andrii Dvorzhak <dvorzhak@gmail.com>
2020-05-03 19:06:22 +0300

    Replace while loops with for loops.

--- task-01/pairs.py
+++ task-01/pairs.py
@@ -11,20 +11,16 @@ def isfloat(value):
 
 
 def find_sums(inputs, sum_to_find=10):
-    i = 0
-    l = len(inputs)
+    l: int = len(inputs)
     pairs = list()
     print("All pairs:")
-    while i < l - 1:
-        j = i + 1
-        while j < l:
+    for i in range(l - 1):
+        for j in range(i + 1, l):
             if inputs[i] + inputs[j] == sum_to_find:
                 print("{0} + {1} = {2}".format(inputs[i], inputs[j], sum_to_find))
                 current_pair = [inputs[i], inputs[j]]
                 current_pair.sort()
                 pairs.append(current_pair)
-            j += 1
-        i += 1
     print("Unique pairs:")
     unique_pairs = set(tuple(current_pair) for current_pair in pairs)
     for current_pair in unique_pairs:
@@ -34,7 +30,7 @@ def find_sums(inputs, sum_to_find=10):
 def main():
     inputs = []
     for current_input in sys.argv[1:]:
-        current_input_parsed = re.findall('^((\"|\'){0,2})([\d\.]*)((\"|\'){0,2})$', current_input)
+        current_input_parsed = re.findall('^((\"|\'){0,2})([\d.]*)((\"|\'){0,2})$', current_input)
         if current_input_parsed and isfloat(current_input_parsed[0][2]):
             if current_input_parsed != "NaN":
                 inputs.append(float(current_input_parsed[0][2]))
